fd12805ecd833f10eda35ae8e0c844680d818838
Fatih NALBANT <fatihnalbantiu@gmail.com>
2021-03-28 23:42:49 +0300

    'ranking'

---
+++ ranking.py
@@ -0,0 +1,26 @@
+largest = None
+smallest = None
+joke = []
+while True: 
+    number = input ("Enter a number : ")
+    if number == "done" : break
+
+    try:
+        numberr=int(number)
+    except:
+        print("ERROR")
+        break
+    joke.append(numberr) 
+
+largest = joke[0]
+for i in joke:
+    if i>largest:
+        largest = i
+print ("Invalid input")
+print ("Maximum is ", largest )
+
+smallest = joke [0]
+for i in joke:
+    if i<smallest:
+        smallest = i
+print ("Minimum is ", smallest)
